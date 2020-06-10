3318d5b19f80e11c53102c26389399b1f0649468
Mahmoud3211 <mahmoudnada5997@gmail.com>
2020-06-10 10:49:39 +0200

    WE DID IT CLASSES CAN BE PREDICTED!!!!!!!

--- Deployment/loadModel.js
+++ Deployment/loadModel.js
@@ -1,19 +1,28 @@
 class loadModel{
+    constructor(){
+        this.model;
+    }
     load(){
         const MODEL_URL = 'http://127.0.0.1:8887/jsModel/model.json';
         const model = tf.loadLayersModel(MODEL_URL);
         return model
     }
 
-    async print(){
-        const model = await this.load(); 
-        console.log(model.summary())
+    async prepareModel(){
+        this.model = await this.load(); 
+        console.log(this.model.summary())
         alert('Model is Loaded')
     }
 
+    async predict(frames) {          
+        
+        const predictedClass = tf.tidy(() => {
+            const pred = this.model.predict(tf.stack(rescaledFrames).expandDims(0));
+            return pred.as1D().argMax()
+        });
 
-
+        
+        return await (await predictedClass.data())[0]
+        //   document.getElementById("prediction").innerText = predictionText;
+      }
 }
-
-
-
